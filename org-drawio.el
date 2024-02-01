6935faeaea0fc7c3acfdd3982bf144a6278d7f1e
kimim <kimi.im@outlook.com>
2024-02-01 19:50:42 +0800

    update melpazoid.yml checkout@v4, change next-line to forward-line

--- org-drawio.el
+++ org-drawio.el
@@ -223,7 +223,7 @@
       ;; skip #+caption, #+name of image
       (if (org-next-line-empty-p)
           (progn (end-of-line) (insert-char ?\n))
-        (while (string-prefix-p "#+" (org-current-line-string (next-line)))))
+        (while (string-prefix-p "#+" (org-current-line-string (forward-line)))))
       ;; convert from drawio to svg asynchronously, thanks to twiddling
       (let ((process (start-process-shell-command "org-drawio" nil script)))
         (set-process-sentinel
